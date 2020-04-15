0d95c14f9569fd8636c5c12144a059eea5526a5e
Dawid <dawid.przyblinski@gmail.com>
2020-04-15 19:09:01 +0200

    przykładowy dataset i proste imputacje

--- szkielet.r
+++ szkielet.r
@@ -12,6 +12,37 @@ imputation_fun_mice <- function(df){
 
 # i tutaj kolejne 4 funkcje, unless nie potrzebują specjalnego trakowania i można je załatwić elipsisem(...)
 
+# delete rows with missing values (można wpisać w ellipsis)
+imp_remove_rows <- function(df){
+  return (na.omit(df))
+}
+
+# impute median within numeric type columns and mode within character type ones 
+imp_mode_median <- function(df){
+  
+  Mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+  }
+  
+  for (i in 1L:length(df)){
+    if (sum(is.na(df[,i])) > 0){
+      if (mode(df[,i]) == 'character'){
+        to_imp <- Mode(df[,i])
+        df[,i][is.na(df[,i])] <- to_imp
+      }
+      else{
+        to_imp <- median(df[,i], na.rm = TRUE) 
+        df[,i][is.na(df[,i])] <- to_imp
+      }
+    }
+  }
+  
+  return(df)
+}
+
+
+
 get_result <- function(dataset, imputation_fun, ...){
   #zmierzyć czas teog ponizej
   imputated_dataset <- imputation_fun(dataset, ...)
@@ -22,3 +53,7 @@ get_result <- function(dataset, imputation_fun, ...){
 }
 
 #PARALLEL ROBI BRRRRR
+
+# example dataset
+options(stringsAsFactors = FALSE)
+data = read.csv("irish.csv")
