5c0e6a95d0692e371b4f2fbd2072e4b6f795262f
Kuba Kosterna <38791658+Kostee@users.noreply.github.com>
2020-04-17 14:06:27 +0200

    Pełen szkielet skryptu (?)

--- szkielet.r
+++ szkielet.r
@@ -1,3 +1,5 @@
+# setwd("C:/Users/ckostern/Desktop/Studia/03 rok II semestr/WB/WB_Prezentacja_softImpute-master")
+# getwd()
 set.seed(123)
 
 library(mice)
@@ -5,6 +7,7 @@ library(missForest)
 library(VIM)
 
 DFT_REPO_DATASET_DIR = './dependencies/datasets'
+getwd()
 
 #' Reads dataset based on id from directory
 #' @param openml_id int. Should be present in dataset_dir
@@ -52,7 +55,7 @@ read_all_datasets <- function(dataset_dir = DFT_REPO_DATASET_DIR){
   return(datasets_combined)
 }
 
-
+# IMPUTATION FUNCTIONS
 
 imputation_fun_mice <- function(df){
   init <- mice(data, maxit=0) 
@@ -60,7 +63,7 @@ imputation_fun_mice <- function(df){
   predM <- init$predictorMatrix
   imputed <- mice(data, method=meth, predictorMatrix=predM, m=5)
   completed <- complete(imputed)
-  return(completed)
+  return(completed[1]) # UWAGA! Kuba dodal tu [1], zeby potem miec juz czysty dataframe
 }
 
 imputation_fun_vim <- function(df){
@@ -70,11 +73,9 @@ imputation_fun_vim <- function(df){
   return(imputed)
 }
 
-
 imputation_fun_missForest <- function(df){
   return(missForest(df)$ximp)
 }
-
 
 imputation_remove_rows <- function(df){
   return (na.omit(df))
@@ -105,26 +106,113 @@ imputation_mode_median <- function(df){
 }
 
 
-get_result <- function(dataset, imputation_fun, ...){
-  #zmierzyÄ‡ czas teog ponizej
-  imputated_dataset <- imputation_fun(dataset, ...)
-  #tworzenie modelu ALBO danie nienauczonego modelu jako argument? - random forest nie jest zÅ‚y
-  #liczymy miary rÃ³Å¼ne
-  #ewentualnie zapisywac do pliku wyniki
-  #return tabelka z miarami  + czas imputacji
+# KUBA TU ZACZYNA
+
+# splits dataset into train and test sets, taking into account train size
+train_test_split <- function(dataset, train_size){
+  smp_size <- floor(train_size * nrow(dataset))
+  typeof(smp_size)
+  
+  train_ind <- sample(seq_len(nrow(dataset)), size = smp_size)
+  
+  train <- dataset[train_ind, ]
+  test <- dataset[-train_ind, ]
+  
+  return (list(train, test))
+}
+#train <- train_test_split(data_all[[1]], 0.75)[1] # getting train
+#test <- train_test_split(data_all[[1]], 0.75)[2] # getting test
+
+
+# METRICS FUNCTIONS
+
+get_confusion_matrix <- function(test, pred){
+  return (table(Truth = test, Prediction = pred))
+}
+
+confusion_matrix_values <- function(confusion_matrix){
+  TP <- confusion_matrix[2,2]
+  TN <- confusion_matrix[1,1]
+  FP <- confusion_matrix[1,2]
+  FN <- confusion_matrix[2,1]
+  return (c(TP, TN, FP, FN))
+}
+
+accuracy <- function(confusion_matrix){
+  conf_matrix <- confusion_matrix_values(confusion_matrix)
+  return((conf_matrix[1] + conf_matrix[2]) / (conf_matrix[1] + conf_matrix[2] + conf_matrix[3] + conf_matrix[4]))
+}
+
+precision <- function(confusion_matrix){
+  conf_matrix <- confusion_matrix_values(confusion_matrix)
+  return(conf_matrix[1]/ (conf_matrix[1] + conf_matrix[3]))
+}
+
+recall <- function(confusion_matrix){
+  conf_matrix <- confusion_matrix_values(confusion_matrix)
+  return(conf_matrix[1] / (conf_matrix[1] + conf_matrix[4]))
+}
+
+f1 <- function(confusion_matrix){
+  conf_matrix <- confusion_matrix_values(confusion_matrix)
+  rec <- recall(confusion_matrix)
+  prec <- precision(confusion_matrix)
+  return(2 * (rec * prec) / (rec + prec))
 }
 
 
-#PARALLEL ROBI BRRRRR
+library(rpart) # for classification tree
+get_result <- function(dataset, imputation_fun, name_of_target){
+  start_time = Sys.time() # start to measure time
+  
+  imputated_dataset <- imputation_fun(dataset) 
+  
+  train_test <- train_test_split(imputated_dataset, 0.8)
+  train <- as.data.table(train_test[1])
+  test <- as.data.table(train_test[2])
+  
+  tree_model <- rpart(train[[name_of_target]] ~ ., data = train,
+                      method = "class", control = rpart.control(cp = 0))
+  
+  y_pred <- as.data.frame(predict(tree_model, test, type = "class"))
+  
+  confusion_matrix <- get_confusion_matrix(test[[name_of_target]], y_pred[,1])
+  print(confusion_matrix)
+  
+  accuracy_v <- accuracy(confusion_matrix)
+  precision_v <- precision(confusion_matrix)
+  recall_v <- recall(confusion_matrix)
+  f1_v <- f1(confusion_matrix)
+  
+  classification_report <- data.frame(accuracy_v, precision_v,
+                                      recall_v, f1_v)
+  colnames(classification_report) <- c("accuracy", "precision",
+                                                 "recall", "f1")
+  print(classification_report)
+  
+  end_time = Sys.time() # end measuring time
+  print (end_time - start_time) # time for operation
+  
+  # TODO: trzy printy na returna
+}
+# example: get_result(data_all[[2]], imputation_fun_vim, "Utility")
 
-#IMPUTACJE ROBIÄ„ BRRRRR
 
-data_all <- read_all_datasets()
-data <- data_all[[1]]
+### WYWO£ANIE
+data_all <- read_all_datasets() # TODO: przynajmniej u Kuby problemy z datasetami 3,4,8
+# data <- data_all[[2]] <= example: getting second dataset
 
-imputation_fun_mice(data)
-imputation_fun_vim(data)
-imputation_fun_missForest(data)
-imputation_remove_rows(data)
-imputation_mode_median(data)
+#PARALLEL ROBI BRRRRR [TODO bo nie wiem jak]
 
+#IMPUTACJE ROBI¥ BRRRRR
+
+imputations <- list(imputation_fun_vim, imputation_fun_missForest,
+                imputation_remove_rows, imputation_mode_median, imputation_fun_mice)
+targets <- list("class", "Utility", NULL, NULL, "Class", "match", "job", NULL)
+# TODO: automatyzacja targetów, tak jak z read_datasets()
+
+for (imputation in imputations){
+  for(i in 1:8){
+    get_result(data_all[[i]], imputation, targets[i])
+  }
+}
