e8db0b3501fc5138cc24f0e4378a932e7da23244
hania <zdula616@gmail.com>
2020-05-11 22:58:03 +0200

    save dfs

--- parallel.r
+++ parallel.r
@@ -1,104 +1,73 @@
-fortunes::fortune(10)
+set.seed(123)
 
-?Map
+source('./imputations_wrapped_functions.r')
+source('./metrics_functions.r')
+source('./reading_datasets_functions.r')
 
 library(parallelMap)
 library(microbenchmark)
 
-# adapted from: https://mllg.github.io/batchtools/articles/batchtools.html#example-1-approximation-of-pi
-
-approximate_pi <- function(n) {
-  nums <- matrix(runif(2 * n), ncol = 2)
-  d <- sqrt(nums[, 1]^2 + nums[, 2]^2)
-  4 * mean(d <= 1)
-}
-RNGkind("L'Ecuyer-CMRG")
-set.seed(1410)
-
-approximate_pi(5)
-
-lapply(rep(1e5, 100), approximate_pi)
-
-# under the hood parallel::mclapply
-# również parallelStartMPI
-parallelStartMulticore(4, show.info = TRUE)
-# parallelStart()
-parallelLapply(rep(1e5, 10), approximate_pi)
-parallelLapply(rep(1e5, 3), function(x){parallelLapply(rep(1e5, 10), approximate_pi)})
-
-parallelStop()
-
-parallelStartMulticore(4, show.info = FALSE)
-microbenchmark(lapply = lapply(rep(1e5, 100), approximate_pi), 
-               parallelLapply = parallelLapply(rep(1e5, 100), approximate_pi), 
-               times = 1)
-parallelStop()
-parallelStart()
-microbenchmark(lapply = lapply(1L:3, function(x){lapply(rep(1e5, 100), approximate_pi)}), 
-               parallelLapply = parallelLapply(rep(1e5, 3), function(x){parallelLapply(rep(1e5, 100), approximate_pi)}), 
-               times = 1)
-
-
-# batchtools ----------------------
-library(batchtools)
-
-# ułatwienie: btlapply
-
-registry <- makeRegistry(file.dir = "./registry", seed = 15390)
-getDefaultRegistry()
-# setDefaultRegistry()
-# clearRegistry()
-batchMap(fun = approximate_pi, n = rep(1e5, 10))
-getJobTable()
-submitJobs(resources = list(walltime = 3600, memory = 1024))
-getStatus()
-
-microbenchmark(lapply = lapply(rep(1e5, 100), approximate_pi), 
-               batchtools = function(){
-                 clearRegistry()
-                 batchMap(fun = approximate_pi, n = rep(1e5, 100))
-                 submitJobs(resources = list(walltime = 3600, memory = 1024))
-                 waitForJobs()
-                 cat('dupa')
-               }, 
-               times = 1, unit = 's')
-
-
-# readRDS("./file_registry/user.function.rds")
-
-aaa <- function(dataset, na.omit, ...){
-  return(fun(x, ...))
+if(!dir.exists('./times')){
+  dir.create("./times")
 }
 
-loadResult(1)
-readRDS("./file_registry/results/1.rds")
-makeClusterFunctionsMulticore(ncpus = 4)
-clearRegistry()
-batchMap(fun = approximate_pi, n = rep(1e5, 500))
-getJobTable()
-submitJobs(resources = list(walltime = 3600, memory = 1024))
-waitForJobs()
-
-make_some_parallels <- function(x){
-  clearRegistry()
-  cat(x)
-  batchMap(fun = approximate_pi, n = rep(1e5, 5))
-  submitJobs(resources = list(walltime = 3600, memory = 1024))
-  waitForJobs()
-  ret <- lapply(1L:10, loadResult)
-  return(ret)
+if(!dir.exists('./imputed')){
+  dir.create("./imputed")
 }
 
-clearRegistry()
-batchMap(fun = make_some_parallels,x=1L:3)
-submitJobs(resources = list(walltime = 3600, memory = 1024))
-getJobTable()
-waitForJobs()
+save_imputed_datasets <- function(dataset_list, imputation_fun){
+  
+  dataset <- dataset_list$dataset
+  name_of_target <- dataset_list$target
+  
+  # imputation
+  imputation_start = Sys.time() # start to measure time
+  imputated_dataset <- imputation_fun(dataset) 
+  imputation_stop = Sys.time() # end measuring time
+  
+  dataset_list$imp_time <- imputation_stop - imputation_start
+  
+  dataset_list$dataset <- imputated_dataset
+  
+  imp_method_name <- deparse(substitute(imputation_fun))
+  
+  dataset_list$imp_method <- imp_method_name
+  
+  dfdir <- paste0('./imputed/', imp_method_name)
+  
+  dfname <- paste0('./imputed/', imp_method_name,'/', dataset_list$id, '.rds')
+  
+  if(!dir.exists(dfdir)){
+    dir.create(dfdir)
+  }
+  
+  saveRDS(dataset_list, dfname)
+}
 
 
-# makeClusterFunctionsSSH
-#?parallelStartBatchtools
-library(future.batchtools)
-plan("batchtools_interactive")
-batchMap(fun = approximate_pi, n = rep(1e5, 10))
-clearRegistry()
+library(parallelMap)
+rdzenie <- parallel::detectCores()
+parallelStartMulticore(rdzenie, show.info = TRUE)
+
+# reading datasets from OpenML's edits
+data_all <- read_all_datasets()
+
+# imputations and targets preparation
+imputations <- list(imputation_fun_vim,
+                    imputation_fun_missForest,
+                    imputation_remove_rows,
+                    imputation_mode_median,
+                    imputation_fun_mice)
+
+starttime <- Sys.time()
+parallelLapply(data_all, function(x){save_imputed_datasets(x,imputation_remove_rows)}, impute.error = function(x){'ERROR'})
+parallelLapply(data_all, function(x){save_imputed_datasets(x,imputation_mode_median)}, impute.error = function(x){'ERROR'})
+parallelLapply(data_all, function(x){save_imputed_datasets(x,imputation_fun_mice)}, impute.error = function(x){'ERROR'})
+parallelLapply(data_all, function(x){save_imputed_datasets(x,imputation_fun_missForest)}, impute.error = function(x){'ERROR'})
+parallelLapply(data_all, function(x){save_imputed_datasets(x,imputation_fun_vim)}, impute.error = function(x){'ERROR'})
+endtime <- Sys.time()
+endtime - starttime
+
+
+
+
