15b8ac3664b3a571bc055cdcfc0b12638644e3a9
hania <zdula616@gmail.com>
2020-05-15 11:30:27 +0200

    dataset info

--- app.R
+++ app.R
@@ -3,6 +3,10 @@ library(shiny)
 final_results <- readRDS('./final_results.RDS')
 f <- unlist(final_results, recursive = FALSE)
 
+final_results2 <- readRDS('./final_results_2.RDS')
+f2 <- unlist(final_results2, recursive = FALSE)
+
+
 models <- unique(sapply(f, function(x){x$mod_mehtod}, simplify = TRUE))
 mod <- sapply(models, function(x){substr(x, 23, nchar(x))}, simplify = TRUE, USE.NAMES = FALSE)
 names(models) <- mod
@@ -37,7 +41,9 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-            textOutput("cmt"),
+            textOutput("dane"),
+            tableOutput("danetab"),
+           textOutput("cmt"),
            tableOutput("cm"),
            tableOutput("cr"),
            plotOutput("roc"),
@@ -57,19 +63,46 @@ server <- function(input, output, session) {
         l
         })
     
+    l2 <- reactive({
+        x <- f2[sapply(f, function(x, val){x$mod_mehtod == val}, val = input$mod, simplify = T)]
+        x <- x[sapply(x, function(x, val){x$imp_method == val}, val = input$imp, simplify = T)]
+        df_ <- x[sapply(x, function(x, val){x$dataset_id == val}, val = input$id, simplify = T)]
+        l2 <- df_[[1]]
+        l2
+    })
+    
     require(PRROC)
+    
+    output$dane <- renderText("Info")
+    
+    output$danetab <- renderTable({
+        x <- l()
+        id <- x$dataset_id
+        
+        j <- jsonlite::read_json(paste0('dependencies/datasets/openml_dataset_', id, "/dataset.json"))
+        
+        w <- c(as.integer(j$number_of_instances), 
+                  as.integer(j$number_of_features),
+                  as.integer(j$number_of_missing),
+                  (100 * j$number_of_missing/(j$number_of_instances * j$number_of_features)),
+                  as.double(x$imputation_time, units ='secs'))
+        out <- as.matrix(w) %>% as.data.frame()
+        rownames(out) <- c("number of instances", "number of features", "number of missing", "% of missing", "imputation time [s]")
+        out
+    }, rownames = T)
+    
     
     output$cmt <- renderText("Confusion matrix")
     
     output$cm <- renderTable({
-        x <- l()
+        x <- l2()
         x$confusion_matrix %>% as.matrix()
-        as.data.frame.matrix(prop.table(x$confusion_matrix))
+        as.data.frame.matrix(x$confusion_matrix)
     
         }, rownames = T)
     
     output$cr <- renderTable({
-        x <- l()
+        x <- l2()
         x$classification_report
     })
     
